defdf8426a43b2f99ae4a2c1ce5cdb7ab9645175
Israel Edeh <israeledeh11@gmail.com>
2025-06-28 19:46:56 +0100

    feat: implement POST endpoint for logout functionality with cookie clearing

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -21,3 +21,24 @@ export async function GET() {
     );
   }
 }
+
+export async function POST() {
+  try {
+    const response = NextResponse.json(
+      { message: "Logout successful" },
+      { status: StatusCodes.OK }
+    );
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
